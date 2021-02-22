d95a8e401838fbae5316f8fb658e0103c8e31437
neatdisorder <nachocast92@gmail.com>
2021-02-22 10:10:56 -0300

    Change BrowserRouter to HashRouter for allowing a working build to be created.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/home/Home';
@@ -17,7 +17,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
           <div id="header-container">
             <Header />
           </div>
@@ -30,7 +30,7 @@ class App extends React.Component {
             <Route path='/clock' component={ClockApp} />
           </Switch>
           <Footer />
-      </BrowserRouter>
+      </HashRouter>
     );
   };
 };
